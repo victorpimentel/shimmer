ad6bd993053953501507368df47dbbec6799e8f0
Ben Gummer <ben@shimmerapp.com>
2009-10-04 14:24:29 +1100

    API code only requires appName and appVariant parameter for relevant API methods (ie. version.info and not application.list)

--- flows/api.php
+++ flows/api.php
@@ -15,86 +15,80 @@ function convertFieldName($table,$field,$type=1) {
 $appName    = $_GET['appName'];
 $appVariant = $_GET['appVariant'];
 if (!isset($appVariant)) $appVariant = "";
-if ( isset($appName) ) {
-	$app = $Shimmer->apps->appFromNameAndVariant($appName, $appVariant);
-	if ($app) {
-		$versionsTable = $app['versions_table'];
-		if ($versionsTable) {
-			$method = strtolower($_GET['method']);
-			if ($method == "version.info") {
-			
-				// CHOOSE WHICH FIELDS TO SELECT
-				$chosenColumns = array();
-				$allowedFilters = array("version","build","size","signature","notes","users","downloads");
-				$filterPairs = array(
-									array("downloads","download_count"),
-									array("size",     "bytes"),
-									array("users",    "user_count")
-								);
-				$filterString = $_GET['fields'];
-				if ( isset($filterString) && strlen($filterString)>0 ) {
-					$userFilters = explode(',',strtolower($filterString));
-					if ( count($userFilters)>0 ) {
-						if (!in_array("version",$userFilters)) array_push($userFilters,"version");
-						foreach ($userFilters as $key => $userFilter) {
-							if (strlen($userFilter)>0 && in_array($userFilter,$allowedFilters) && $userFilter != "users") {
-								array_push( $chosenColumns, convertFieldName($filterPairs,strval($userFilter),1) );
-							}
-						}
+
+$method = strtolower($_GET['method']);
+if ($method == "version.info") {
+	if ($app = $Shimmer->apps->appFromNameAndVariant($appName, $appVariant)) {
+		// CHOOSE WHICH FIELDS TO SELECT
+		$chosenColumns = array();
+		$allowedFilters = array("version","build","size","signature","notes","users","downloads");
+		$filterPairs = array(
+							array("downloads","download_count"),
+							array("size",     "bytes"),
+							array("users",    "user_count")
+						);
+		$filterString = $_GET['fields'];
+		if ( isset($filterString) && strlen($filterString)>0 ) {
+			$userFilters = explode(',',strtolower($filterString));
+			if ( count($userFilters)>0 ) {
+				if (!in_array("version",$userFilters)) array_push($userFilters,"version");
+				foreach ($userFilters as $key => $userFilter) {
+					if (strlen($userFilter)>0 && in_array($userFilter,$allowedFilters) && $userFilter != "users") {
+						array_push( $chosenColumns, convertFieldName($filterPairs,strval($userFilter),1) );
 					}
 				}
-				
-				// Search Setup (only return live versions, etc)
-				$searchConditions = array( 'fields'=>$chosenColumns, 'onlyLive'=>true );
-				if ($userFilters && in_array("users",$userFilters)) $searchConditions['includeUserCount'] = true;
-				
-				// Version Search
-				$versionCriteria = $_GET['appVersion'];
-				if (!isset($versionCriteria)) $versionCriteria = "all";
-				if ($versionCriteria == "latest") {
-					$searchConditions['limit'] = 1;
-				} else if ($versionCriteria == "oldest") {
-					$searchConditions['flipDirection'] = true;
-					$searchConditions['limit'] = 1;
-				} else if ($versionCriteria != "all") {
-					$searchConditions['increment'] = $versionCriteria;
-				}
-				
-				$searchedVersions = $Shimmer->versions->versions($app, $searchConditions);
-				if ($searchedVersions!==false) {
-					$resp['ok'] = true;
-					$returnVersions = array();
-					foreach ($searchedVersions as $version) {
-						$currentVersion = array();
-						foreach ($version as $currentColumn => $currentValue) {
-							if (isset($currentValue)) {
-								$userColumnName = convertFieldName($filterPairs,strval($currentColumn),2);
-								if (in_array($userColumnName,$allowedFilters) && $currentColumn!="version") $currentVersion[$userColumnName] = $currentValue;
-							}
-						}
-						$rowVersion = strval($version['version']);
-						$currentVersion['version'] = $rowVersion;
-						array_push($returnVersions,$currentVersion);
+			}
+		}
+	
+		// Search Setup (only return live versions, etc)
+		$searchConditions = array( 'fields'=>$chosenColumns, 'onlyLive'=>true );
+		if ($userFilters && in_array("users",$userFilters)) $searchConditions['includeUserCount'] = true;
+	
+		// Version Search
+		$versionCriteria = $_GET['appVersion'];
+		if (!isset($versionCriteria)) $versionCriteria = "all";
+		if ($versionCriteria == "latest") {
+			$searchConditions['limit'] = 1;
+		} else if ($versionCriteria == "oldest") {
+			$searchConditions['flipDirection'] = true;
+			$searchConditions['limit'] = 1;
+		} else if ($versionCriteria != "all") {
+			$searchConditions['increment'] = $versionCriteria;
+		}
+	
+		$searchedVersions = $Shimmer->versions->versions($app, $searchConditions);
+		if ($searchedVersions!==false) {
+			$resp['ok'] = true;
+			$returnVersions = array();
+			foreach ($searchedVersions as $version) {
+				$currentVersion = array();
+				foreach ($version as $currentColumn => $currentValue) {
+					if (isset($currentValue)) {
+						$userColumnName = convertFieldName($filterPairs,strval($currentColumn),2);
+						if (in_array($userColumnName,$allowedFilters) && $currentColumn!="version") $currentVersion[$userColumnName] = $currentValue;
 					}
-					$resp['versions'] = $returnVersions;
 				}
-			} else if ($method == "application.list") {
-				$returnedApplications = array();
-				foreach ($Shimmer->apps->list as $app) {
-					$currentApplication = array(
-						'name'        => $app['name'],
-						'variant'     => $app['variant'],
-						'displayName' => $app['name'] . ($app['variant'] ? ': ' . $app['variant'] : ''),
-						'id'          => $app['id']
-					);
-					array_push($returnedApplications, $currentApplication);
-				}
-				$resp['applications'] = $returnedApplications;
-				$resp['ok'] = true;
-			} else $resp['reason'] = "No method supplied";
-		} else $resp['reason'] = "Could not open table";
+				$rowVersion = strval($version['version']);
+				$currentVersion['version'] = $rowVersion;
+				array_push($returnVersions,$currentVersion);
+			}
+			$resp['versions'] = $returnVersions;
+		}
 	} else $resp['reason'] = "App does not exist";
-} else $resp['reason'] = "No app supplied";
+} else if ($method == "application.list") {
+	$returnedApplications = array();
+	foreach ($Shimmer->apps->list as $app) {
+		$currentApplication = array(
+			'name'        => $app['name'],
+			'variant'     => $app['variant'],
+			'displayName' => $app['name'] . ($app['variant'] ? ': ' . $app['variant'] : ''),
+			'id'          => $app['id']
+		);
+		array_push($returnedApplications, $currentApplication);
+	}
+	$resp['applications'] = $returnedApplications;
+	$resp['ok'] = true;
+} else $resp['reason'] = "No method supplied";
 
 if ($resp['ok']===false && !$resp['reason']) $resp['reason'] = "Unknown error";
 
